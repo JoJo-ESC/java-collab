e57ae3447977e0ea195989177594a664068a4cbe
JoJo-ESC <148640793+JoJo-ESC@users.noreply.github.com>
2025-05-30 11:16:45 -0400

    Friend change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from user 1!");
+        System.out.println("Friend added this line!");
     }
 }
